0ffc6ba9939055142573d223b5e63dc9e6aaf57c
Azmeer Sohail Khan <i212977>
2025-02-18 14:05:29 +0500

    Add GitHub Actions workflow for Vercel deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Flask App at Vercel lets go"
 
 if __name__ == '__main__':
     app.run(debug=True)
